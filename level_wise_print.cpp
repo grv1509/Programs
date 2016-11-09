631c153913e57f690983843a72c30ac3f7efaa50
grv1509 <gaurav.verma1509@gmail.com>
2016-11-09 18:30:49 +0000

    Tree

---
+++ level_wise_print.cpp
@@ -0,0 +1,54 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+struct Node{
+	int data;
+	struct Node *left;
+	struct Node *right;
+	
+	Node(int x){
+		data = x;
+		left = NULL;
+		right = NULL;	
+	}	
+};
+
+
+void printSpiral(Node *root){
+	queue<Node*> q;
+	Node *curr = root;
+	
+	q.push(curr);
+	
+	while(!q.empty()){
+		Node *temp = q.front();
+		if(temp->left) q.push(temp->left);
+		if(temp->right)q.push(temp->right);
+		cout << temp->data << " ";
+		q.pop();
+	}
+	
+}
+void print(Node *root){
+	if(root == NULL) return;
+	print(root->left);
+	cout << root-> data << " ";
+	print(root->right);
+	
+}
+
+int main(){
+	
+	Node *root = new Node(1);
+
+	root->left = new Node(2);
+	root->right = new Node(3);
+	root->left->left = new Node(5);
+	root->left->right = new Node(6);
+	root->right->left = new Node(67);
+	root->right->left->right = new Node(9);
+	root->right->left->left = new Node(10);
+	
+	printSpiral(root);
+	return 0;	
+}
---
+++ tree.cpp
@@ -0,0 +1,39 @@
+#include<iostream>
+using namespace std;
+
+struct Node{
+	int data;
+	struct Node *left;
+	struct Node *right;
+	
+	Node(int x){
+		data = x;
+		left = NULL;
+		right = NULL;	
+	}	
+};
+
+
+void print(Node *root){
+	if(root == NULL) return;
+	print(root->left);
+	cout << root-> data << " ";
+	print(root->right);
+	
+}
+
+int main(){
+	
+	Node *root = new Node(1);
+
+	root->left = new Node(2);
+	root->right = new Node(3);
+	root->left->left = new Node(5);
+	root->left->right = new Node(6);
+	root->right->left = new Node(67);
+	root->right->left->right = new Node(9);
+	root->right->left->left = new Node(10);
+	
+	print(root);
+	return 0;	
+}
